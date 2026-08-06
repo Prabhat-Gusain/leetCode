fe3c4bd0bc8edee5dfc83ef20fcbd2bf1b2c4bde
Prabhat_Gusain <136064267+Prabhat-Gusain@users.noreply.github.com>
2026-08-06 14:07:17 +0530

    Time: 2 ms (77.84%) | Memory: 7.9 MB (38.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int fib(int n) {
+        // if(n<=1)
+        // {
+        //     return n;
+        // }
+        // else
+        // {
+        //     return fib(n-2)+fib(n-1);
+        // }
+
+        int t0=0,t1=1,s=0,i;
         if(n<=1)
         {
             return n;
         }
-        else
+        
+        for(i=2;i<=n;i++)
         {
-            return fib(n-2)+fib(n-1);
+            s=t0+t1;
+            t0=t1;
+            t1=s;
         }
+        return s;
     }
 };
