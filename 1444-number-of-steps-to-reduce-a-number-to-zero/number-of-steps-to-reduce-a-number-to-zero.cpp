c3ca1dbe30d5d66b65f85f4dbde0175ba4574884
Prabhat_Gusain <136064267+Prabhat-Gusain@users.noreply.github.com>
2026-08-11 16:47:07 +0530

    Time: 1 ms (1.39%) | Memory: 8.1 MB (6.55%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps=0;
+        while(num>0)
+        {
+            if(num%2==0)
+            {
+                steps++;
+                num/=2;
+            }
+            else
+            {
+                steps++;
+                num-=1;
+            }
+        }
+        return steps;
+    }
+};
